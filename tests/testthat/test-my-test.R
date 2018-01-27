64278f0773c57758daccab90f6247d0c7607a356
annakrystalli <annakrystalli@googlemail.com>
2018-01-27 21:52:02 +0000

    add pkgreview checks

--- tests/testthat/test-my-test.R
+++ tests/testthat/test-my-test.R
@@ -27,9 +27,29 @@ test_that("structure-correct", {
 })
 
 
-test_that("review-initialised-correctly", {
+test_that("review-files-initialised-correctly", {
     expect_equal(sort(list.files(review_dir)),
                  sort(c("index.Rmd", "pkgreview.md", "R",
                    #"rdflib-review.Rproj",
                    "README.md")))
+})
+
+meta <- devtools:::github_remote(pkg_repo)
+pkg_dir <- file.path(paste0(review_dir, "/../", meta$repo))
+pkgdata <- pkgreview_getdata(pkg_dir)
+
+test_that("check-pkgdata", {
+    expect_equal(pkgdata$pkg_repo, "annakrystalli/rdflib")
+    expect_equal(pkgdata$index_url, "https://annakrystalli.github.io/annakrystalli/rdflib/index.nb.html")
+    expect_equal(pkgdata$review_repo, "annakrystalli/rdflib-review")
+    expect_equal(pkgdata$pkgreview_url, "https://github.com/annakrystalli/rdflib-review/blob/master/pkgreview.md")
+    expect_equal(pkgdata$issue_url, "https://github.com/ropensci/onboarding/issues/169")
+    expect_equal(pkgdata$number, 169)
+    expect_equal(pkgdata$whoami, setNames("annakrystalli", "gh_username"))
+    expect_equal(pkgdata$whoami_url, "https://github.com/annakrystalli")
+    expect_equal(pkgdata$pkg_dir, pkg_dir)
+    expect_equal(pkgdata$Package, "rdflib")
+    expect_equal(pkgdata$repo, "rdflib")
+    expect_equal(pkgdata$site, "https://annakrystalli.github.io/rdflib/")
+    expect_false(pkgdata$Rmd)
 })
