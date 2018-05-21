c128d822a9396f525ab95c0a5f6816bf66ee144d
Sam Albers <Sam.Albers@gov.bc.ca>
2018-05-21 16:36:31 -0700

    fixed rev_calls to actually work

--- R/rev.R
+++ R/rev.R
@@ -1,17 +1,15 @@
 #' Create a dataframe of functions that a called and called by
 
-rev_calls <- function(package){
+rev_calls <- function(path = "."){
   
   check_if_installed(package = package)
   
-  call_data <- create_igraph_object()
-  
-  d_call_data <- igraph::degree(call_data)
+  call_data <- create_package_igraph()
   
   
-  in_degree <- as.data.frame(d_call_data[["inDegree"]])
+  in_degree <- as.data.frame(igraph::degree(call_data, mode = c("in")))
   in_degree$f_name <- rownames(in_degree)
-  out_degree <- as.data.frame(d_call_data[["outDegree"]])
+  out_degree <- as.data.frame(igraph::degree(call_data, mode = c("out")))
   out_degree$f_name <- rownames(out_degree)
   
   degree_df <- merge(in_degree, out_degree)
