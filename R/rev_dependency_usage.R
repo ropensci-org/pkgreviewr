a8cd4ecf9baf29b40ab4906b255c427179a95b25
maurolepore <maurolepore@gmail.com>
2018-05-21 17:00:14 -0700

    Test rev_dependency_usage().

--- R/rev_dependency_usage.R
+++ R/rev_dependency_usage.R
@@ -1,3 +1,5 @@
+# TODO: Refactor to remove dependencies.
+
 #' Review dependency usage: Count used functions from external packages.
 #' 
 #' @inheritParams functionMap::map_r_package
@@ -10,11 +12,13 @@
 #' rev_dependency_usage(path = ".")
 #' }
 rev_dependency_usage <- function(path, include_base = FALSE) {
-  map <- functionMap::map_r_package(path, include_base)
-  functionMap::node_df(map) %>% 
+  map <- functionMap::map_r_package(path, include_base)$node_df
+  map %>% 
   dplyr::filter(!own) %>%
   dplyr::filter(ID != "::") %>% 
-  tidyr::separate(ID, into = c("package", "fn"), sep="::") %>% 
+  tidyr::separate(ID, into = c("package", "fn"), sep = "::") %>% 
   dplyr::group_by(package) %>% 
-  dplyr::summarize(n = n())
+  dplyr::summarize(
+    n = length(unique(fn)), functions = paste0(fn, collapse = ", ")
+  )
 }
