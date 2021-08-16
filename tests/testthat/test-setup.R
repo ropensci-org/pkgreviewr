a8d1a1b904090a15f1af2fa8daa1da0412711728
annakrystalli <annakrystalli@googlemail.com>
2021-08-16 09:57:59 +0100

    remove superfluous test

--- tests/testthat/test-setup.R
+++ tests/testthat/test-setup.R
@@ -4,14 +4,3 @@ test_that("check-rstudio", {
     expect_error(pkgreviewr:::check_rstudio())
 })
 
-test_that("missing-git-config-throws-error", {
-    check_global_git <- pkgreviewr:::check_global_git
-    mockery::stub(check_global_git,
-                  "try", function(){
-                      error <- TRUE
-                      class(error) <- "try-error"
-                      error
-                  })
-    expect_error(check_global_git())
-    rm(check_global_git)
-})
