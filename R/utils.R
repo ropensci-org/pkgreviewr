2a8ec6191738c2c5c43889b1b1aea08580ccd96f
Maëlle Salmon <maelle.salmon@yahoo.se>
2025-02-17 17:35:14 +0100

    chore: rm rstudio check

--- R/utils.R
+++ R/utils.R
@@ -1,23 +1,3 @@
-check_rstudio <- function() {
-    # check if rstudio available
-    if (!rstudioapi::isAvailable()) {
-        stop(strwrap(paste("pkreviewr is designed to work with Rstudio version" ,
-                           "1.0 and above.", "Please install or launch in Rstudio to proceed.",
-                           "Visit <https://www.rstudio.com/products/rstudio/download/>",
-                           " to download the latest version"), prefix = "\n"))
-    }
-
-    # check rstudio version > 1.0.0
-    rs_version <- rstudioapi::getVersion()
-    if (rs_version < "1.0.0") {
-        stop(strwrap(paste("pkreviewr is designed to work with notebooks in Rstudio version" ,
-                           "1.0 and above.",
-                           "Please update to RStudio version 1.0 or greater to proceed.",
-                           "You are running RStudio",
-                           as.character(rs_version)), prefix = "\n"))
-    }
-}
-
 # modified from usethis::use_git
 use_git_pkgrv <- function (path = ".", message = "Initial commit") {
     if (uses_git_pkgrv(path)) {
